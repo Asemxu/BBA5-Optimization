6f9fc872b1a40e10e67cfa0ac3dfb1612a161ae0
Asemxu <hijo72@hotmail.com>
2021-09-13 13:25:59 -0500

    ADD:validations in forms reset password , checkout y reset password profile

--- src/AppBBA5.js
+++ src/AppBBA5.js
@@ -3,23 +3,29 @@ import { Provider } from 'react-redux';
 
 import { store } from './Redux/Store';
 import { Routers } from './Routers/Routers';
-import TagManager from 'react-gtm-module';
-import ReactPixel from 'react-facebook-pixel';
 import './Styles/styles.scss';
 
-const tagManagerArgs = {
-  gtmId: 'GTM-WJDR4S5',
-};
 
-TagManager.initialize(tagManagerArgs);
 
-const advancedMatching = { em: 'some@email.com' };
-const options = {
-  autoConfig: true, // set pixel's autoConfig
-  debug: false, // enable logs
-};
-ReactPixel.init('176593354576731', advancedMatching, options);
-ReactPixel.pageView();
+import ('react-gtm-module').then(TagManager=>{
+  const tagManagerArgs = {
+    gtmId: 'GTM-WJDR4S5',
+  };
+  
+  TagManager.initialize(tagManagerArgs);
+
+});
+
+import('react-facebook-pixel').then(ReactPixel=>{
+  const advancedMatching = { em: 'some@email.com' };
+  const options = {
+    autoConfig: true, // set pixel's autoConfig
+    debug: false, // enable logs
+  };
+  ReactPixel.default.init('176593354576731', advancedMatching, options);
+  ReactPixel.default.pageView();
+  
+});
 
 export const AppBBA5 = () => {
   return (
